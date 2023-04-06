c6f9f203bfffc1c7ebff803f41de1b4e3d16fc0c
saver13203 <115583141+saver13203@users.noreply.github.com>
2023-04-06 13:37:19 -0500

    Update and rename Settlements.java to Settlement.java

---
+++ KingdomBuilderCode/Settlement.java
@@ -0,0 +1,19 @@
+public class Settlement()
+{
+  private int numLeft;
+  private Point position;
+  private boolean bordersWater;
+  private boolean bordersMountain;
+  private boolean bordersCastle;
+  public static BufferedImage[]settlementImages;
+  private Tile tile;
+  public Settlement()
+  {
+  }
+  
+  public int getSettlements() {}
+  public int resetSettlements() {}
+  public void buildSettlements(Point pos) {}
+  public void mouseAction(MouseEvent e) {}
+  public void moveSettlements (Point pos, Point pos2) {}
+}
