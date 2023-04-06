955f5c8fc5108d6d2ab68db829ef6ddde46407be
saver13203 <115583141+saver13203@users.noreply.github.com>
2023-04-06 13:31:32 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,28 @@
+public class Constants
+{
+    public final int WORKERS;
+    public final int CITIZENS;
+    public final int DISCOVERERS;
+    public final int KNIGHTS;
+    public final int LORDS;
+    public final int FARMERS;
+    public final int MERCHANTS;
+    public final int FISHERMAN;
+    public final int MINERS;
+    public final int HERMITS;
+      
+    public final int GRASS;
+    public final int CANYON;
+    public final int DESERT;
+    public final int FLOWER;
+    public final int FOREST;
+    public final int WATER;
+    public final int MOUNTAIN;
+    public final int CASTLE;
+    public final int LOCATION;
+    
+   public Constants()
+   {
+   }
+   
+}
