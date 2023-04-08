57d5756d05c0e13808bc12419d53b60a6dfe68a5
rogerw <54212947+notAssoc@users.noreply.github.com>
2023-04-07 19:11:02 -0500

    Update StartPanel.java

--- KingdomBuilderCode/src/StartPanel.java
+++ KingdomBuilderCode/src/StartPanel.java
@@ -6,8 +6,19 @@ public class StartPanel
 {
 	private boolean startGame;
 	private String startMessage;
-	private BufferedImage TileScreen;
+	private BufferedImage startScreen;
 	private BufferedImage RuleBoard;
+	
+	public StartPanel() {
+		try {
+			startScreen = ImageIO.read(StartPanel.class.getResource("/pictures/StartScreen.jpg"));
+		}
+		catch (Exception e) {
+			System.out.println("Error");
+		}
+		addMouseListener(this);
+		repaint();
+	}
 	
 	public String getStartMessage()
 	{
@@ -21,14 +32,30 @@ public class StartPanel
 	
 	public void paint(Graphics g)
 	{
-		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 	}
 	
 	public void rotateBoards()
 	{
 		
 	}
-	
 	public void mouseClicked(MouseEvent e) {}
-
+	
+	public void MousePressed(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		if(x>=532 && x<=1052 && y>=445 && y<=577) {
+			try {
+				GameFrame a = new GameFrame("Kingdom Builder");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Error");
+			}
+		}
+	}
+	
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	
 }
