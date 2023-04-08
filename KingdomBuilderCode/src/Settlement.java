1b80f110fd134bc5f5aed466a7b0eaecc4f36581
eela <117680659+ellaawei@users.noreply.github.com>
2023-04-07 20:05:39 -0500

    Update Settlement.java

--- KingdomBuilderCode/src/Settlement.java
+++ KingdomBuilderCode/src/Settlement.java
@@ -1,4 +1,12 @@
-public class Settlement()
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.*;
+
+
+public class Settlement
 {
   private int numLeft;
   private Point position;
@@ -6,13 +14,19 @@ public class Settlement()
   private boolean bordersMountain;
   private boolean bordersCastle;
   public static BufferedImage[]settlementImages;
-  private Tile tile;
+  private Tiles tile;
   public Settlement()
   {
   }
   
-  public int getSettlements() {}
-  public int resetSettlements() {}
+  public int getSettlements() 
+  {
+    return 0;
+  }
+  public int resetSettlements() 
+  {
+    return 0;
+  }
   public void buildSettlements(Point pos) {}
   public void mouseAction(MouseEvent e) {}
   public void moveSettlements (Point pos, Point pos2) {}
