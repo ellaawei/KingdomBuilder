29a6b45a94e4a53bc68b06d9f87f3440977eb41c
rogerw <54212947+notAssoc@users.noreply.github.com>
2023-04-11 12:59:20 -0500

    Update Constants.java

--- KingdomBuilderCode/src/Constants.java
+++ KingdomBuilderCode/src/Constants.java
@@ -1,28 +1,27 @@
-public class Constants
-{
-    public final int WORKERS;
-    public final int CITIZENS;
-    public final int DISCOVERERS;
-    public final int KNIGHTS;
-    public final int LORDS;
-    public final int FARMERS;
-    public final int MERCHANTS;
-    public final int FISHERMAN;
-    public final int MINERS;
-    public final int HERMITS;
+
+public class Constants {
+	public final int WORKERS = 0;
+    public final int CITIZENS = 1;
+    public final int DISCOVERERS = 2;
+    public final int KNIGHTS = 3;
+    public final int LORDS = 4;
+    public final int FARMERS = 5;
+    public final int MERCHANTS = 6;
+    public final int FISHERMAN = 7;
+    public final int MINERS = 8;
+    public final int HERMITS = 9;
       
-    public final int GRASS;
-    public final int CANYON;
-    public final int DESERT;
-    public final int FLOWER;
-    public final int FOREST;
-    public final int WATER;
-    public final int MOUNTAIN;
-    public final int CASTLE;
-    public final int LOCATION;
+    public final int GRASS = 10;
+    public final int CANYON = 11;
+    public final int DESERT = 13;
+    public final int FLOWER = 14;
+    public final int FOREST = 15;
+    public final int WATER = 16;
+    public final int MOUNTAIN = 17;
+    public final int CASTLE = 18;
+    public final int LOCATION = 19;
     
-   public Constants()
-   {
-   }
-   
+    public Constants() {
+    	
+    }
 }
