62de13cde860a70e7249d2a1de8ba249b8d28e38
rogerw <54212947+notAssoc@users.noreply.github.com>
2023-04-10 20:50:42 -0500

    Update GamePanel.java

--- KingdomBuilderCode/src/GamePanel.java
+++ KingdomBuilderCode/src/GamePanel.java
@@ -7,61 +7,110 @@ import java.awt.image.*;
 import javax.imageio.*;
 
 import javax.swing.JPanel;
-public class GamePanel extends JPanel implements MouseMotionListener, MouseListener
-{
+
+public class GamePanel extends JPanel implements MouseMotionListener, MouseListener {
 	private int x;
 	private int y;
-	
-	public static BufferedImage images, board1, board2, board3, board4, board5, board6, board7, board8;
+
+	private BufferedImage images;
+	private BufferedImage objCardsPage, tokensPage, terrainDeck;
+	private boolean clickedObjectiveCards, clickedViewTokens, clickedTerrain;
+	private ArrayList<BufferedImage> objectives;
+	private ArrayList<BufferedImage> currentObjectiveCards;
+	private Card c;
+	private Board b;
+
 	public GamePanel() throws IOException {
+		objectives = new ArrayList<BufferedImage>();
+		currentObjectiveCards = new ArrayList<BufferedImage>();
+		c = new Card();
+		b = new Board();
 		images = ImageIO.read(this.getClass().getResource("/Image/board.png"));
-		board1 = ImageIO.read(this.getClass().getResource("/Image/Board1.png"));
-		board2 = ImageIO.read(this.getClass().getResource("/Image/Board2.png"));
-		board3 = ImageIO.read(this.getClass().getResource("/Image/Board3.png"));
-		board4 = ImageIO.read(this.getClass().getResource("/Image/Board4.png"));
+		terrainDeck = ImageIO.read(this.getClass().getResource("/Image/TerrainDeck.png"));
+		objCardsPage = ImageIO.read(this.getClass().getResource("/Image/ObjectiveCardsPage.png"));
+		tokensPage = ImageIO.read(this.getClass().getResource("/Image/TokensPage.png"));
 		addMouseListener(this);
 		repaint();
 	}
+
 	public void paint(Graphics g) {
 		g.drawImage(images, 0, 0, getWidth(), getHeight(), null);
-		g.drawImage(board1, -111, 45, 750, 445, null);
-		g.drawImage(board2, 270, 45, 750, 445, null);
-		g.drawImage(board3, -111, 393, 750, 445, null);
-		g.drawImage(board4, 270, 393, 750, 445, null);
+		g.drawImage(terrainDeck, 954, 27, 163, 220, null);
+		b.drawBoard(g);
+		
+		c.drawObjectiveCards(g);
+		if (c.getHasTerrainCardsLeft() && clickedTerrain) {
+			c.drawTerrainCards(g);
+			c.drawDiscard(g);
+		}
+		if (c.getHasTerrainCardsLeft() == false) {
+			c.refill();
+		}
+
+		if (clickedObjectiveCards) {
+			g.drawImage(objCardsPage, 0, 0, getWidth(), getHeight(), null);
+			return;
+		}
+		if (clickedViewTokens) {
+			g.drawImage(tokensPage, 0, 0, getWidth(), getHeight(), null);
+		}
 	}
-	@Override
+
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	@Override
+
 	public void mousePressed(MouseEvent e) {
-		
+		int x = e.getX();
+		int y = e.getY();
+		if (x >= 703 && x <= 876 && y >= 35 && y <= 85) {
+			clickedObjectiveCards = true;
+		} else {
+			clickedObjectiveCards = false;
+		}
+		if (x >= 1320 && x <= 1493 && y >= 162 && y <= 224) {
+			clickedViewTokens = true;
+		} else {
+			clickedViewTokens = false;
+		}
+		if (x >= 954 && x <= 1117 && y >= 27 && y <= 247) {
+			clickedTerrain = true;
+			if (c.getHasTerrainCardsLeft()) {
+				c.removeTerrainCard();
+			}
+		}
+		repaint();
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
