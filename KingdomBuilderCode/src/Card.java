cbebf11680115c0e1d4411500383c8f72c8d7693
eela <117680659+ellaawei@users.noreply.github.com>
2023-04-11 16:05:40 -0500

    Update Card.java

--- KingdomBuilderCode/src/Card.java
+++ KingdomBuilderCode/src/Card.java
@@ -1,136 +1,110 @@
-import javax.swing.*;
-import java.util.*;
-import java.io.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.*;
-import javax.imageio.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.*;
 
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
 
-public class GamePanel extends JPanel implements MouseMotionListener, MouseListener {
-	private int x;
-	private int y;
+public class Card {
+	private ArrayList<BufferedImage> objectiveCards;
+	private ArrayList<BufferedImage> terrainCards;
+	private ArrayList<BufferedImage> discard;
 
-	private BufferedImage board;
-	private BufferedImage objCardsPage, tokensPage, terrainDeck;
-	public static boolean clickedObjectiveCards, clickedViewTokens, clickedTerrain, clickedFinishTurn = false;
-	private ArrayList<BufferedImage> objectives;
-	private ArrayList<BufferedImage> currentObjectiveCards;
-	private Card c;
-	private Board b;
+	private boolean hasCardsLeft;
+	private BufferedImage workers, citizens, discoverers, knights, lords, farmers, merchants, fisherman, miners,
+			hermits;
+	private BufferedImage grass, canyon, desert, flower, forest;
 
-	public GamePanel() throws IOException {
-		objectives = new ArrayList<BufferedImage>();
-		currentObjectiveCards = new ArrayList<BufferedImage>();
-		c = new Card();
-		b = new Board();
-		board = ImageIO.read(this.getClass().getResource("/Image/board.png"));
-		terrainDeck = ImageIO.read(this.getClass().getResource("/Image/TerrainDeck.png"));
-		objCardsPage = ImageIO.read(this.getClass().getResource("/Image/ObjectiveCardsPage.png"));
-		tokensPage = ImageIO.read(this.getClass().getResource("/Image/TokensPage.png"));
-		addMouseListener(this);
-		repaint();
+	public Card() throws IOException {
+		objectiveCards = new ArrayList<>();
+		terrainCards = new ArrayList<>();
+		discard = new ArrayList<>();
+		workers = ImageIO.read(this.getClass().getResource("/Image/ObjectiveWorkers.png"));
+		citizens = ImageIO.read(this.getClass().getResource("/Image/ObjectiveCitizens.png"));
+		discoverers = ImageIO.read(this.getClass().getResource("/Image/ObjectiveDiscoverers.png"));
+		knights = ImageIO.read(this.getClass().getResource("/Image/ObjectiveKnights.png"));
+		lords = ImageIO.read(this.getClass().getResource("/Image/ObjectiveLords.png"));
+		farmers = ImageIO.read(this.getClass().getResource("/Image/ObjectiveFarmers.png"));
+		merchants = ImageIO.read(this.getClass().getResource("/Image/ObjectiveMerchants.png"));
+		fisherman = ImageIO.read(this.getClass().getResource("/Image/ObjectiveFisherman.png"));
+		miners = ImageIO.read(this.getClass().getResource("/Image/ObjectiveMiners.png"));
+		hermits = ImageIO.read(this.getClass().getResource("/Image/ObjectiveHermits.png"));
+
+		objectiveCards.add(workers);
+		objectiveCards.add(citizens);
+		objectiveCards.add(discoverers);
+		objectiveCards.add(knights);
+		objectiveCards.add(lords);
+		objectiveCards.add(farmers);
+		objectiveCards.add(merchants);
+		objectiveCards.add(fisherman);
+		objectiveCards.add(miners);
+		objectiveCards.add(hermits);
+
+		grass = ImageIO.read(this.getClass().getResource("/Image/TerrainGrass.png"));
+		canyon = ImageIO.read(this.getClass().getResource("/Image/TerrainCanyon.png"));
+		desert = ImageIO.read(this.getClass().getResource("/Image/TerrainDesert.png"));
+		flower = ImageIO.read(this.getClass().getResource("/Image/TerrainFlower.png"));
+		forest = ImageIO.read(this.getClass().getResource("/Image/TerrainForest.png"));
+		for (int i = 0; i < 5; i++) {
+			terrainCards.add(grass);
+			terrainCards.add(canyon);
+			terrainCards.add(desert);
+			terrainCards.add(flower);
+			terrainCards.add(forest);
+		}
+		Collections.shuffle(objectiveCards);
+		Collections.shuffle(terrainCards);
 	}
 
-	public void paint(Graphics g) 
+	public void drawTerrainCards(Graphics g) 
 	{
-		g.drawImage(board, 0, 0, getWidth(), getHeight(), null);
-		g.drawImage(terrainDeck, 954, 27, 163, 220, null);
-		c.drawObjectiveCards(g);
-		b.drawBoard(g);
-		if (c.getHasTerrainCardsLeft() && clickedTerrain) 
+		g.drawImage(terrainCards.get(0), 1068, 316, 149, 215, null);
+		if(GamePanel.clickedFinishTurn)
 		{
-			c.drawTerrainCards(g);
-			c.drawDiscard(g);
+			discard.add(0,terrainCards.get(0));
 		}
-		if (c.getHasTerrainCardsLeft() == false) 
+	}
+
+	public void drawDiscard(Graphics g) 
+	{
+		if(GamePanel.clickedFinishTurn)
 		{
-			c.refill();
-		}
-
-		if (clickedObjectiveCards) 
-		{
-			g.drawImage(objCardsPage, 0, 0, getWidth(), getHeight(), null);
-			return;
-		}
-		if (clickedViewTokens)
-		{
-			g.drawImage(tokensPage, 0, 0, getWidth(), getHeight(), null);
-			//return;
-		}
-		
-		if(clickedFinishTurn && !clickedViewTokens && !clickedObjectiveCards)
-		{
-			c.drawDiscard(g);
-			//return;
+			g.drawImage(discard.get(0), 1129, 27, 163, 220, null);
 		}
 	}
 
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+	public void removeTerrainCard() {
+		terrainCards.remove(0);
 	}
 
-	public void mousePressed(MouseEvent e) {
-		int x = e.getX();
-		int y = e.getY();
-		if (x >= 703 && x <= 876 && y >= 35 && y <= 85) {
-			clickedObjectiveCards = true;
-		} else {
-			clickedObjectiveCards = false;
+	public ArrayList<BufferedImage> getTerrainCards() {
+		return terrainCards;
+	}
+
+	public void drawObjectiveCards(Graphics g) {
+		g.drawImage(objectiveCards.get(0), 95, 55, 200, 250, null);
+		g.drawImage(objectiveCards.get(1), 295, 55, 200, 250, null);
+		g.drawImage(objectiveCards.get(2), 495, 55, 200, 250, null);
+	}
+
+	public boolean getHasTerrainCardsLeft() {
+		hasCardsLeft = !terrainCards.isEmpty();
+		return hasCardsLeft;
+	}
+
+	public void setHasCardLeft(boolean bool) {
+		hasCardsLeft = bool;
+	}
+	
+	public void refill() {
+		for (int i = 0; i < 5; i++) {
+			terrainCards.add(grass);
+			terrainCards.add(canyon);
+			terrainCards.add(desert);
+			terrainCards.add(flower);
+			terrainCards.add(forest);
 		}
-		if (x >= 1320 && x <= 1493 && y >= 162 && y <= 224) {
-			clickedViewTokens = true;
-			clickedFinishTurn = false;
-		} else {
-			clickedViewTokens = false;
-		}
-		if (x >= 954 && x <= 1117 && y >= 27 && y <= 247) 
-		{
-			clickedTerrain = true;
-			if (c.getHasTerrainCardsLeft()) 
-			{
-				c.removeTerrainCard();
-			}
-		}
-		
-		if(x >= 1305 && x <= 1510 && y >= 65 && y <= 140)
-		{
-			clickedFinishTurn = true;
-			
-		}
-		repaint();
+		Collections.shuffle(terrainCards);
 	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
