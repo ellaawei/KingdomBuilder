350532f85c18814c19ab55b1c9e7570db7c45257
saver13203 <115583141+saver13203@users.noreply.github.com>
2023-04-12 13:40:08 -0500

    Update Player.java

--- KingdomBuilderCode/src/Player.java
+++ KingdomBuilderCode/src/Player.java
@@ -13,7 +13,7 @@ public class Player()
         locationTiles = new ArrayList<>();
     }
     public boolean isNear(Tile locationTile) {}
-    public int tokensLeft();
+    public int tokensLeft() {}
 
 
     public String getColor() {
