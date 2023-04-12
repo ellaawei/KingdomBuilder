5beb1dede25f64c1e36d40b194d1b653823cf8ef
saver13203 <115583141+saver13203@users.noreply.github.com>
2023-04-12 13:09:45 -0500

    Update Player.java

--- KingdomBuilderCode/src/Player.java
+++ KingdomBuilderCode/src/Player.java
@@ -1,1 +1,35 @@
 
+public class Player()
+{
+    private String myColor;
+    private boolean isFirst;
+    private ArrayList<Tiles> locationTiles;
+    private Settlement settlemets[40];
+    private int score;
+    private BufferedImage currentSettlement;
+
+    pubilc Player()
+    {
+
+    }
+    public boolean isNear(Tile locationTile) {}
+    public int tokensLeft();
+
+
+    public String getColor() {
+        return myColor;
+    }
+    public boolean isFirst() {
+        return isFirst;
+    }
+    public ArrayList<Tiles> getLocationTiles() {}
+    public void setColor(String color) {
+        myColor = color;
+    }
+    public void sellsFirst(boolean first) {}
+    public void setLocationTiles(ArrayList<Tiles>locations) {}
+    public int getScore() {
+        return score;;
+    }
+    public void calculateScore() {}
+}
