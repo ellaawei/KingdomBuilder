c323e682db65a3423be5e3fb3098932f8d003468
iated <54212947+notAssoc@users.noreply.github.com>
2023-04-21 13:02:37 -0500

    Update Board.java

--- KingdomBuilderCode/src/Board.java
+++ KingdomBuilderCode/src/Board.java
@@ -1,27 +1,46 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
 public class Board {
-	private BufferedImage board1, board2, board3, board4, board5, board6, board7, board8;
-	private ArrayList<BufferedImage> boards;
+	private SubBoard board1, board2, board3, board4, board5, board6, board7, board8;
+	private ArrayList<SubBoard> boards;
 	Point pos;
+	Point anchor = new Point(67, 87); // Hexgon starting point
+	private Hexagon[][] Hexadjacent;
+	private ArrayList<Hexagon> houses = new ArrayList<Hexagon>();
+	
+	class SubBoard
+	{
+		public BufferedImage image;
+		public String file;
+		
+		SubBoard(BufferedImage image, String file)
+		{
+			this.image = image;
+			this.file = file;
+		}
+	}
 	
 	public Board() throws IOException {
-		boards = new ArrayList<BufferedImage>();
-		board1 = ImageIO.read(this.getClass().getResource("/Image/Board1.png"));
-		board2 = ImageIO.read(this.getClass().getResource("/Image/Board2.png"));
-		board3 = ImageIO.read(this.getClass().getResource("/Image/Board3.png"));
-		board4 = ImageIO.read(this.getClass().getResource("/Image/Board4.png"));
-		board5 = ImageIO.read(this.getClass().getResource("/Image/Board5.png"));
-		board6 = ImageIO.read(this.getClass().getResource("/Image/Board6.png"));
-		board7 = ImageIO.read(this.getClass().getResource("/Image/Board7.png"));
-		board8 = ImageIO.read(this.getClass().getResource("/Image/Board8.png"));
+		boards = new ArrayList<SubBoard>();
+		board1 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board1.png")), "One.txt");
+		board2 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board2.png")), "Two.txt");
+		board3 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board3.png")), "Three.txt");
+		board4 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board4.png")), "Four.txt");
+		board5 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board5.png")), "Five.txt");
+		board6 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board6.png")), "Six.txt");
+		board7 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board7.png")), "Seven.txt");
+		board8 = new SubBoard(ImageIO.read(this.getClass().getResource("/Image/Board8.png")), "Eight.txt");
 		boards.add(board1);
 		boards.add(board2);
 		boards.add(board3);
@@ -32,7 +51,70 @@ public class Board {
 		boards.add(board8);
 		Collections.shuffle(boards);
 		
+		Hexadjacent = new Hexagon[20][20];
+		RenderUtil r = new RenderUtil();
+		double HexLen = 22.7;
+		double altRad = r.getAltRadius(HexLen);
+		
+		for(int row = 0; row<20; row++)
+		{
+			for(int col = 0; col<20; col++)
+			{
+				double cx, cy;
+				
+				if(row % 2 == 0)
+				{
+					cx = anchor.x + HexLen + r.getHorizontalShift(HexLen)*col;
+					cy = anchor.y + HexLen + row* (r.getVerticalShift(HexLen)+0.7);
+				}
+				else
+				{
+					cx = anchor.x + HexLen + r.getHorizontalShift(HexLen)*col + altRad;
+					cy = anchor.y + HexLen + row* (r.getVerticalShift(HexLen)+0.7);
+				}
+				
+				Polygon p = r.createHexagon(cx ,cy, HexLen);
+				Hexadjacent[row][col] = new Hexagon(p);
+			}
+		}
+		
+		Scanner input = new Scanner(new File(boards.get(0).file));
+		for (int row = 0; row < 10; row++)
+		{
+			for (int col = 0; col < 10; col++)
+			{
+				Hexadjacent[row][col].color = input.nextLine();
+			}
+		}
+		input = new Scanner(new File(boards.get(1).file));
+		for (int row = 0; row < 10; row++)
+		{
+			for (int col = 10; col < 20; col++)
+			{
+				Hexadjacent[row][col].color = input.nextLine();
+			}
+		}
+		input = new Scanner(new File(boards.get(2).file));
+		for (int row = 10; row < 20; row++)
+		{
+			for (int col = 1; col < 10; col++)
+			{
+				Hexadjacent[row][col].color = input.nextLine();
+			}
+		}
+		input = new Scanner(new File(boards.get(3).file));
+		for (int row = 10; row < 20; row++)
+		{
+			for (int col = 10; col < 20; col++)
+			{
+				Hexadjacent[row][col].color = input.nextLine();
+			}
+		}
+		
+		int  i = 0;
 	}
+	
+	
 	public void selectHex(int x, int y) {
 		
 	}
@@ -40,10 +122,33 @@ public class Board {
 		
 	}
 	public void drawBoard(Graphics g) {
-		g.drawImage(boards.get(0), -111, 44, 775, 445, null);
-		g.drawImage(boards.get(1), 283, 44, 775, 445, null);
-		g.drawImage(boards.get(2), -111, 393, 775, 445, null);
-		g.drawImage(boards.get(3), 283, 393, 775, 445, null);
+		g.drawImage(boards.get(0).image, -111, 44, 775, 445, null);
+		g.drawImage(boards.get(1).image, 283, 44, 775, 445, null);
+		g.drawImage(boards.get(2).image, -111, 393, 775, 445, null);
+		g.drawImage(boards.get(3).image, 283, 393, 775, 445, null);
+		
+		for (Hexagon h : houses)
+		{
+			Polygon p = new Polygon(h.xpoints(), h.ypoints(), 6);
+			g.fillPolygon(p);
+			g.setColor(Color.BLACK);
+			g.drawPolygon(p);
+		}
+	}
+	
+	public void mouseClick(Point p)
+	{
+		for (int row = 0; row < 20; row++)
+		{
+			for (int col = 0; col < 20; col++)
+			{
+				Hexagon h = Hexadjacent[row][col];
+				Polygon pol = new Polygon(h.xpoints(), h.ypoints(), 6);
+				if(!pol.contains(p)) continue;
+				
+				houses.add(h.getHouse(""));
+			}
+		}
 	}
 	
 }
