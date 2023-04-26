52c2ef52f8bac2a544fc1e903ae4c45d3c2f78e8
eela <117680659+ellaawei@users.noreply.github.com>
2023-04-26 17:47:45 -0500

    no bc summer and melody can u pls work?

--- KingdomBuilderCode/src/Player.java
+++ KingdomBuilderCode/src/Player.java
@@ -15,24 +15,24 @@ public class Player
     //private Settlement settlements[];
     private int score;
     private BufferedImage currentSettlement;
-    public int settlementNum;
-    public int x = 960, y = 515;
+  //  public int settlementNum;
+    //public int x = 960, y = 515;
 
     public Player()
     {
     	//settlements = new Settlement[40];
         locationTiles = new ArrayList<>();
-        settlementNum = 40;
+      //  settlementNum = 40;
     }
     public boolean isNear(Tiles locationTile) 
     {
     	return false;
     }
     
-    public int getSettlementNum()
-    {
-    	return settlementNum;
-    }
+//    public int getSettlementNum()
+//    {
+//    	return settlementNum;
+//    }
     
     public int tokensLeft() {
     	return 0;
@@ -60,28 +60,28 @@ public class Player
     	terrainColor =color;
     }
     
-    public void settlementsLeft(int x)
-    {
-    	settlementNum = settlementNum - x;
-    }
-    public void paintPlayer(Graphics g)
-    {
-    	if(x == 1263 && y == 515)
-    	{
-    		g.setColor(Color.white);
-    	}
-    	else
-    	{
-    		g.setColor(Color.black);
-    	}
-		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		g.drawString(getSettlementNum() + "", x, y);
-//		g.setColor(Color.white);
-//		g.drawString(players[1].getSettlementNum() + "", 1263, 517);
-//		g.setColor(Color.black);
-//		g.drawString(players[2].getSettlementNum() + "", 960, 775);
-//		g.drawString(players[0].getSettlementNum() + "", 1265, 777);
-    }
+//    public void settlementsLeft(int x)
+//    {
+//    	settlementNum = settlementNum - x;
+//    }
+////    public void paintPlayer(Graphics g)
+//    {
+//    	if(x == 1263 && y == 515)
+//    	{
+//    		g.setColor(Color.white);
+//    	}
+//    	else
+//    	{
+//    		g.setColor(Color.black);
+//    	}
+//		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
+//		g.drawString(getSettlementNum() + "", x, y);
+////		g.setColor(Color.white);
+////		g.drawString(players[1].getSettlementNum() + "", 1263, 517);
+////		g.setColor(Color.black);
+////		g.drawString(players[2].getSettlementNum() + "", 960, 775);
+////		g.drawString(players[0].getSettlementNum() + "", 1265, 777);
+//    }
     public void sellsFirst(boolean first) {}
     public void setLocationTiles(ArrayList<Tiles>locations) {}
     public int getScore() {
