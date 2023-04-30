99153a0032bda3f3e70c80fcc64609647459be73
eela <117680659+ellaawei@users.noreply.github.com>
2023-04-30 17:14:54 -0500

    slay

--- KingdomBuilderCode/src/GamePanel.java
+++ KingdomBuilderCode/src/GamePanel.java
@@ -19,41 +19,42 @@ public class GamePanel extends JPanel implements MouseMotionListener, MouseListe
 	private ArrayList<BufferedImage> currentObjectiveCards;
 	private Card c;
 	private Board b;
+	public static Players players;
+	
 	public enum clickType{none, terrain, finishedTurn, viewTokens, objectiveCards};
 	public static clickType click;
 	private static boolean terrainClicked = false;
-	public Color col;
-	public int P1Houses, P2Houses, P3Houses, P4Houses;
 	
 	public GamePanel() throws IOException {
 		objectives = new ArrayList<BufferedImage>();
 		currentObjectiveCards = new ArrayList<BufferedImage>();
 		c = new Card();
 		b = new Board();
+		players = new Players();
 		board = ImageIO.read(this.getClass().getResource("/pictures/board.png"));
 		terrainDeck = ImageIO.read(this.getClass().getResource("/pictures/TerrainDeck.png"));
 		objCardsPage = ImageIO.read(this.getClass().getResource("/pictures/ObjectiveCardsPage.png"));
 		tokensPage = ImageIO.read(this.getClass().getResource("/pictures/TokensPage.png"));
 		clickedTerrain = false;
-		P1Houses = 40;
-		P2Houses = 40;
-		P3Houses = 40;
-		P4Houses = 40;
 		addMouseListener(this);
 		repaint();
 	}
 
 	public void paint(Graphics g) 
 	{
-		int count = 0;
-		g.drawImage(board, 0, 0, getWidth(), getHeight(), null);
-		g.drawImage(terrainDeck, 954, 27, 163, 220, null);
+		//g.drawImage(board, 0, 0, getWidth(), getHeight(), null);
+		g.drawImage(board, 0, 0, GameFrame.WIDTH, GameFrame.HEIGHT, null);
+		g.drawImage(terrainDeck, Players.SX, Players.SY, 163, 220, null);
 		c.drawObjectiveCards(g);
 		b.drawBoard(g);
 		if (c.getHasTerrainCardsLeft() && click==clickType.terrain) 
 		{
-			c.drawTerrainCards(g);
+
 		}
+		
+		players.drawPlayers(g);
+		//c.drawTerrainCards(g);
+		
 		if (c.getHasTerrainCardsLeft() == false) 
 		{
 			try {
@@ -74,78 +75,12 @@ public class GamePanel extends JPanel implements MouseMotionListener, MouseListe
 			g.drawImage(tokensPage, 0, 0, getWidth(), getHeight(), null);
 			return;
 		}
+		
 		else if(click==clickType.finishedTurn)
 		{
 			c.drawDiscard(g);
-			Board.activePlayer++;
-			Board.activePlayer%=4;
 			//return;
 		}
-		
-//		if(Board.activePlayer == 0)
-//		{
-//			col = Color.orange;
-//			//players[activePlayer].x = 960;
-//			//players[activePlayer].y = 515;
-//		}
-//		else if(Board.activePlayer == 1)
-//		{
-//			col = Color.black;
-//			//players[activePlayer].x = 950;
-//			//players[activePlayer].y = 775;
-//		}
-//		else if(Board.activePlayer == 2)
-//		{
-//			col = Color.white;
-//			///players[activePlayer].x = 1263;
-//			//players[activePlayer].y = 515;
-//		}
-//		else if(Board.activePlayer == 3)
-//		{
-//			col = Color.blue;
-//			//players[activePlayer].x = 1263;
-//			//players[activePlayer].y = 775;
-//		}
-		
-		if(Board.activePlayer == 0 && Board.players[Board.activePlayer].hexClicked)
-		{
-			P1Houses--;
-			Board.players[Board.activePlayer].hexClicked = false;
-		}
-		if(Board.activePlayer == 1 && Board.players[Board.activePlayer].hexClicked)
-		{
-			P2Houses--;
-			Board.players[Board.activePlayer].hexClicked = false;
-		}
-		if(Board.activePlayer == 2 && Board.players[Board.activePlayer].hexClicked)
-		{
-			P3Houses--;
-			Board.players[Board.activePlayer].hexClicked = false;
-		}
-		if(Board.activePlayer == 3 && Board.players[Board.activePlayer].hexClicked)
-		{
-			P4Houses--;
-			Board.players[Board.activePlayer].hexClicked = false;
-		}
-		
-		g.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		g.setColor(Color.white); //p1
-		g.drawString(P1Houses + "", 960, 515);
-		g.setColor(Color.white); //p2
-		g.drawString(P2Houses + "", 1263, 515);
-		g.setColor(Color.black); //p3
-		g.drawString(P3Houses + "", 960, 775);
-		g.setColor(Color.white); //p4
-		g.drawString(P4Houses + "", 1263, 775);
-		
-//		if(count == 0)
-//		{
-//			P1Houses++;
-//			//P2Houses++;
-//			//P3Houses++;
-//			P4Houses++;
-//			count++;
-//		}
 	}
 
 	public void mouseClicked(MouseEvent e) {
@@ -154,7 +89,6 @@ public class GamePanel extends JPanel implements MouseMotionListener, MouseListe
 	}
 
 	public void mousePressed(MouseEvent e) {
-			
 		int x = e.getX();
 		int y = e.getY();
 		click = clickType.none;
@@ -164,19 +98,18 @@ public class GamePanel extends JPanel implements MouseMotionListener, MouseListe
 		else if (x >= 1320 && x <= 1493 && y >= 162 && y <= 224) {
 			click=clickType.viewTokens;
 		}
-		else if (x >= 954 && x <= 1117 && y >= 27 && y <= 247 && terrainClicked == false) 
+		else if (x >= 954 && x <= 1117 && y >= 27 && y <= 247) 
 		{
 			if (c.getHasTerrainCardsLeft()) 
 			{
 				c.removeTerrainCard();
 			}
-			click=clickType.terrain;
-			terrainClicked = true;
+			c.drawTerrainCards();
 		}
 		else if(x >= 1305 && x <= 1510 && y >= 65 && y <= 140)
 		{
 			click=clickType.finishedTurn;
-			terrainClicked = false;
+			players.clickedFinish();
 		}
 		b.mouseClick(e.getPoint());
 		repaint();
