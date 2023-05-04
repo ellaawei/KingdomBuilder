5289c4cdd0f2c12815a37163bfd49645d472a0dd
eela <117680659+ellaawei@users.noreply.github.com>
2023-05-04 12:55:27 -0500

    Update Card.java

--- KingdomBuilderCode/src/Card.java
+++ KingdomBuilderCode/src/Card.java
@@ -87,12 +87,19 @@ public class Card {
 			terrainCards.remove(0);
 		else
 		{
-			try {
-				refill();
-			} catch (IOException e) {
+			try
+			{
+				for (int i = 0; i < 5; i++) {
+					terrainCards.add(new TerrainCard(ImageIO.read(this.getClass().getResource("/pictures/TerrainGrass.png")), Color.GREEN));
+					terrainCards.add(new TerrainCard(ImageIO.read(this.getClass().getResource("/pictures/TerrainCanyon.png")), new Color(102, 51, 0)));
+					terrainCards.add(new TerrainCard(ImageIO.read(this.getClass().getResource("/pictures/TerrainDesert.png")), Color.YELLOW));
+					terrainCards.add(new TerrainCard(ImageIO.read(this.getClass().getResource("/pictures/TerrainFlower.png")), Color.PINK));
+					terrainCards.add(new TerrainCard(ImageIO.read(this.getClass().getResource("/pictures/TerrainForest.png")), new Color(0, 102, 0)));
+				}
+			}catch(Exception e) {
 				System.out.println("card class");
 			}
-			//Collections.shuffle(terrainCards);
+			Collections.shuffle(terrainCards);
 			terrainCards.remove(0);
 		}
 	}
